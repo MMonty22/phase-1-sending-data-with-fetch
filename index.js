7c04aa67e555f4c91e924db0fab939752e07f453
Michael <michael.c.montgomery@gmail.com>
2022-03-02 17:52:46 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            Accept: 'application/json'
+        }, 
+        body:JSON.stringify({
+            name: "Steve",
+            email: "steve@steve.com"
+        })
+    })
+    .then(resp => resp.json())
+    .then(function (data) {
+        document.querySelector('body').append(data.id)
+    })
+    .catch(function (error) {
+        alert("Error, please wait")
+        console.log(error.message)
+        document.querySelector('body').append(error)
+    })
+}
